c1e5be8ec6886599765ee6e1c73ea804574f9408
Ryan Barry <94549600+ryan-barry-99@users.noreply.github.com>
2023-10-05 21:22:14 -0400

    Update ArmRobot.py

--- ArmRobot.py
+++ ArmRobot.py
@@ -20,9 +20,9 @@ class ArmRobot(ArmRobotKinematics):
         This is where you will define the configuration of your robot with the addLink method
 
         Example Usage:
-            # Add a link called link0 with a revolute joint and length 1 m
+            # Add a link called link0 with a revolute joint and length 1 (m)
             self.link0 = self.addLink(joint_type=REVOLUTE, length=1)
-            # Add a prismatic joint with an initial extended length of 1 m
+            # Add a prismatic joint with an initial extended length of 1 (m)
             self.link1 = self.addLink(joint_type=PRISMATIC, length=1)
         '''
         pass
