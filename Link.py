526f5d6bb20e4a7d3e20ce32ba39367eeb1caf09
Ryan Barry <94549600+ryan-barry-99@users.noreply.github.com>
2023-10-09 22:27:36 -0400

    Update Link.py

--- Link.py
+++ Link.py
@@ -13,13 +13,13 @@ PRISMATIC = 0
 REVOLUTE = 1
 
 class Link:
-    def __init__(self, joint_type, length, theta_fix=0, a=0, alpha_fix=0):
+    def __init__(self, joint_type,theta_fix=0, d=0, a=0, alpha_fix=0):
         self.joint_type = joint_type
         # Rotate about the z_n axis an angle theta_n+1 to make x_n parallel to x_n+1
         self.theta_fix = theta_fix
         self.theta = self.theta_fix
         # Translate along the z_n axis a distance d_n+1 to make x_n and x_n+1 collinear
-        self.d = length
+        self.d = d
         # Translate along the (already rotated) x_n axis at a distance of a_n+1 to 
         # bring the origins of x_n and x_n+1 together
         self.a = a
@@ -34,7 +34,7 @@ class Link:
         if self.joint_type == REVOLUTE:
             self.theta = self.theta_fix + joint_value
         elif self.joint_type == PRISMATIC:
-            self.d = length + joint_value
+            self.d = self.d + joint_value
         else:
             print(f"Invalid joint type at joint {joint}")
             return None
